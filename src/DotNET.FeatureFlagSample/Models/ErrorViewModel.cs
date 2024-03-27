75cea9df86d03de3bdef052d5662aca45408bcd9
binxiong <binxiong.tea@redone.com.my>
2024-03-27 10:21:26 +0800

    Setup new project

---
+++ src/DotNET.FeatureFlagSample/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DotNET.FeatureFlagSample.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
